81c019f6054b68dc077829eff483fef151f801c8
Koirina <143921697+Koirina@users.noreply.github.com>
2023-10-16 22:21:55 +1100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,44 +1,38 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import joblib 
+import joblib
 
-st.title(' Mall Custmer Segmentation Project')
+st.title('Mall Customer Segmentation Project')
 
-def get_inputs():
-    
-    #get user inputs
-    lists = []
-    col_names = ['Gender' , 'Age', 'Annual Income (k$)','Spending Score (1-100)']
-    
-    Gender = st.text_input("Insert Gender")
-    Age = st.number_input('Insert Age')
-    Income = st.number_input('Insert Annual Income')
-    Score = st.number_input('Insert Spending Score [1-100]')
-    
-    lists.append(Gender)
-    lists.append(Age)
-    lists.append(Income)
-    lists.append(Score)
-    exo = pd.DataFrame(lists)
-    exo = exo.transpose()
-    exo.columns = col_names
-    st.write('The entered values are ', Gender, Age,Income,Score) 
-    st.write(exo) 
-#create predict button:
-    st.button("Reset", type="primary")
-    if st.button('Predict'):
-    	reconstructed_model = joblib.load('gmm_pipeline.joblib')        
-    	pred= reconstructed_model.predict(exo)
-    	st.subheader('Predicted Customer Cluster is: ', divider='rainbow')
-    	st.subheader(pred)
-    
-  
-   
-  
-   
-    
+# Function to reset user inputs
+def reset_inputs():
+    st.experimental_rerun()
 
-get_inputs()
+# Function to make predictions
+def predict_cluster(user_data):
+    reconstructed_model = joblib.load('gmm_pipeline.joblib')
+    pred = reconstructed_model.predict(user_data)
+    return pred
 
+# Get user inputs
+st.sidebar.header('User Input')
+gender = st.sidebar.selectbox('Gender', ['Male', 'Female'])
+age = st.sidebar.slider('Age', 18, 100, 25)
+annual_income = st.sidebar.slider('Annual Income (k$)', 0, 200, 50)
+spending_score = st.sidebar.slider('Spending Score (1-100)', 1, 100, 50)
 
+if st.sidebar.button("Reset"):
+    reset_inputs()
+
+if st.sidebar.button('Predict'):
+    user_data = pd.DataFrame({'Gender': [gender], 'Age': [age], 'Annual Income (k$)': [annual_income], 'Spending Score (1-100)': [spending_score]})
+    predicted_cluster = predict_cluster(user_data)
+    st.subheader('Predicted Customer Cluster is:')
+    st.write(predicted_cluster)
+
+# Show user inputs in the main section
+st.write('User Inputs:')
+st.write(f'Gender: {gender}')
+st.write(f'Age: {age}')
+st.write(f'Annual Income (k$): {annual_income}')
+st.write(f'Spending Score (1-100): {spending_score}')
